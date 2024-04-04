a63d1f47d6c74e3048c6dd751c8585bd1a87e5f8
BakerAlblam <gallardo0@outlook.com>
2024-04-04 13:42:32 +0200

    kek

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -31,6 +31,7 @@ import { useState, useTransition } from 'react';
 import { AspectRatioKey, debounce, deepMergeObjects } from '@/lib/utils';
 import { updateCredits } from '@/lib/actions/user.actions';
 import MediaUploader from './MediaUploader';
+import TransformedImage from './TransformedImage';
 
 export const formSchema = z.object({
   title: z.string(),
@@ -117,7 +118,7 @@ const TransformationForm = ({
     );
     setNewTransformation(null);
     startTransition(async () => {
-      //await updateCredits(userId, creditFee);
+      await updateCredits(userId, creditFee);
     });
   };
 
@@ -233,6 +234,14 @@ const TransformationForm = ({
                 type={type}
               />
             )}
+          />
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
           />
         </div>
 
